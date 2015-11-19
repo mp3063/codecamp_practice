ea06528603f1f83cc1698e389c8091f62a61df18
Srdjan Sin Jovanovic <mp4065@gmail.com>
2015-11-19 14:24:11 +0100

    druga funkcija: da sklanja i prikazuje elemente u zavisnosti od unosa u input

--- index.php
+++ index.php
@@ -3,12 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <script src="js/vue.min.js"></script>
+    <link href="css/main.css" rel="stylesheet">
     <title>Proba VUE.JS</title>
 </head>
 <body>
 <div id="sin">
-    <h1>{{poruka}}</h1>
-    <input v-model="poruka">
+    <small v-show="!poruka"><em>You must enter a message</em></small>
+    <br>
+    <textarea v-model="poruka" cols="30" rows="10"></textarea><br>
+    <button v-show="poruka" type="submit">Send message</button>
 </div>
 
 <script src="js/main.js"></script>
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var data = {
-    poruka: "Baaaavoooo"
+    poruka: ""
 }
 new Vue({
     el: "#sin",
