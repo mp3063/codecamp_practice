10698a439fb125436b14cf105a5eab2e7cb91b42
Srdjan Sin Jovanovic <mp4065@gmail.com>
2015-11-20 16:59:23 +0100

    codeCamp: palindrome function

--- index.php
+++ index.php
@@ -12,5 +12,23 @@
 </div>
 
 <script src="js/main.js"></script>
+<script>
+
+    function checkPalindrom(str) {
+        var punct = str.replace(/[.,-\/#!$%\^&\*;:{}=\-_`~()]/g, "").split(
+            ' ').join('').toLowerCase();
+        var string = punct.split('').reverse().join('').replace(
+            /[.,-\/#!$%\^&\*;:{}=\-_`~()]/g, "");
+        if (punct == string) {
+            return console.log(true);
+        }
+        else {
+            return console.log(false);
+        }
+    }
+
+    checkPalindrom("My age is 0, 0 si ega ym.");
+
+</script>
 </body>
 </html>
