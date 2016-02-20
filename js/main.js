4549ab1f237daf5fc584ad8aeef08c40bc8558ff
Srdjan Sin Jovanovic <mp4065@gmail.com>
2016-02-20 03:06:09 +0100

    codeCamp: DNA Pairing - The DNA strand is missing the pairing element. Take each character, get its pair, and return the results as a 2d array.Base pairs are a pair of AT and CG. Match the missing element to the provided character. Return the provided character as the first element in each array. For example, for the input GCG, return [["G", "C"], ["C","G"],["G", "C"]]

--- js/main.js
+++ js/main.js
@@ -1,29 +1,27 @@
-function translate(str) {
-    var consonant = ["a", "e", "i", "o", "u"];
+function pair(str) {
+    var niz = [];
     var splString = str.split("");
-    var counter = 0;
-    var consArray = [];
-    splString.forEach(function (letter) {
-        counter++;
-        for (var i = 0; i < consonant.length; i++) {
-            var suglas = consonant[i];
-            if (letter == suglas) {
-                consArray.push(counter - 1);
+
+    function addPair(letter) {
+        switch (letter) {
+            case "A":
+                niz.push(["A", "T"]);
                 break;
-            }
+            case "T":
+                niz.push(["T", "A"]);
+                break;
+            case "G":
+                niz.push(["G", "C"]);
+                break;
+            case "C":
+                niz.push(["C", "G"]);
+                break;
         }
-    });
-    var firstIndex = consArray[0];
-    if (firstIndex > 0) {
-        var transfer = splString.splice(0, firstIndex);
-        transfer.forEach(function (item) {
-            splString.push(item);
-        });
-        return splString.join("") + "ay";
     }
-    else {
-        return splString.join("") + "way";
-    }
+
+    splString.filter(addPair);
+    return niz;
 }
 
-console.log(translate("glove"));
+console.log(pair("ATCGA"));
+
--- js/test.js
+++
@@ -1,6 +0,0 @@
-var niz = ["srdjan", "romina", "tihana"];
-// var spl = niz.split("");
-// var velikoSlovo = spl[0].toUpperCase();
-var zamenjeno = niz.splice(0, 1, "Srdjan");
-console.log(zamenjeno);
-console.log(niz);
