0df7f225ce268f9fffa1b4719ab4becf337cd157
Srdjan Sin Jovanovic <mp4065@gmail.com>
2016-02-20 11:52:31 +0100

    codeCamp: Missing letters - Find the missing letter in the passed letter range and return it. If all letters are present in the range, return undefined.

--- js/main.js
+++ js/main.js
@@ -1,27 +1,32 @@
-function pair(str) {
-    var niz = [];
+function fearNotLetter(str) {
     var splString = str.split("");
-
-    function addPair(letter) {
-        switch (letter) {
-            case "A":
-                niz.push(["A", "T"]);
-                break;
-            case "T":
-                niz.push(["T", "A"]);
-                break;
-            case "G":
-                niz.push(["G", "C"]);
-                break;
-            case "C":
-                niz.push(["C", "G"]);
-                break;
+    var range = [];
+    splString.forEach(function (letter) {
+        range.push(letter.charCodeAt(0));
+    });
+    var start = range[0];
+    var last = range[range.length - 1];
+    var ran = [];
+    while (start <= last) {
+        ran.push(start++);
+    }
+    function missing(a, b) {
+        if (b < range.length - 1) {
+            var c = range[b + 1] - a;
+            if (c !== 1) {
+                return a;
+            }
         }
     }
 
-    splString.filter(addPair);
-    return niz;
+    var miss = range.filter(missing);
+    if (miss.length > 0) {
+        return String.fromCharCode(miss[0] + 1);
+    }
+    else {
+        return undefined;
+    }
 }
 
-console.log(pair("ATCGA"));
+console.log(fearNotLetter("abce"));
 
