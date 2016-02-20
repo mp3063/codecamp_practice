6c7397434c1149774ba47c745cab0aa4cb7be30e
Srdjan Sin Jovanovic <mp4065@gmail.com>
2016-02-20 01:52:31 +0100

    codeCamp: Pig Latin - Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an "ay". If a word begins with a vowel you just add "way" to the end.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,29 @@
-function myReplace(str, before, after) {
-    var bef = before.split("");
-    var af = after.split("");
-    var replace = new RegExp(before, "i");
-    if (bef[0].toLowerCase() === bef[0]) {
-        return str.replace(replace, after);
+function translate(str) {
+    var consonant = ["a", "e", "i", "o", "u"];
+    var splString = str.split("");
+    var counter = 0;
+    var consArray = [];
+    splString.forEach(function (letter) {
+        counter++;
+        for (var i = 0; i < consonant.length; i++) {
+            var suglas = consonant[i];
+            if (letter == suglas) {
+                consArray.push(counter - 1);
+                break;
+            }
+        }
+    });
+    var firstIndex = consArray[0];
+    if (firstIndex > 0) {
+        var transfer = splString.splice(0, firstIndex);
+        transfer.forEach(function (item) {
+            splString.push(item);
+        });
+        return splString.join("") + "ay";
     }
     else {
-        var firstLetter = af[0].toUpperCase();
-        af.splice(0, 1, firstLetter);
-        var res = af.join("");
-        return str.replace(replace, res);
+        return splString.join("") + "way";
     }
 }
 
-console.log(
-    myReplace("Let us get back to more Coding", "Coding", "algorithms"));
+console.log(translate("glove"));
