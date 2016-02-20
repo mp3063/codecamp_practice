ce71a5d83638d3b26a2515e8fcd711bbb2731801
Srdjan Sin Jovanovic <mp4065@gmail.com>
2016-02-20 12:54:24 +0100

    codeCamp: Boo who - Check if a value is classified as a boolean primitive. Return true or false. Boolean primitives are true and false.

--- js/main.js
+++ js/main.js
@@ -1,32 +1,14 @@
-function fearNotLetter(str) {
-    var splString = str.split("");
-    var range = [];
-    splString.forEach(function (letter) {
-        range.push(letter.charCodeAt(0));
-    });
-    var start = range[0];
-    var last = range[range.length - 1];
-    var ran = [];
-    while (start <= last) {
-        ran.push(start++);
-    }
-    function missing(a, b) {
-        if (b < range.length - 1) {
-            var c = range[b + 1] - a;
-            if (c !== 1) {
-                return a;
-            }
+function boo(bool) {
+    if (bool === true || bool === false) {
+        switch (bool) {
+            case true:
+                return true;
+            case false:
+                return true;
         }
     }
-
-    var miss = range.filter(missing);
-    if (miss.length > 0) {
-        return String.fromCharCode(miss[0] + 1);
-    }
-    else {
-        return undefined;
-    }
+    return false;
 }
 
-console.log(fearNotLetter("abce"));
+console.log(boo("false"));
 
