dacad153c80cd3b2ea5125e910faf39e52aebfbb
SrdjanSin <mp4065@gmail.com>
2017-01-30 00:01:42 +0100

    codeCamp: Nesting For Loops

--- js/main.js
+++ js/main.js
@@ -1,21 +1,19 @@
 // Instructions
-// Declare and initialize a variable total to 0.
-// Use a for loop to add the value of each element of the myArr array to total.
-// Example
-var ourArr = [9, 10, 11, 12];
-var ourTotal = 0;
+// Modify function multiplyAll
+// so that it multiplies the product variable by each number in the sub-arrays of arr
 
-for (var i = 0; i < ourArr.length; i++) {
-    ourTotal += ourArr[i];
+function multiplyAll(arr) {
+    var product = 1;
+    // Only change code below this line
+    for (var i = 0; i < arr.length; i++) {
+        for (var j = 0; j < arr[i].length; j++) {
+            product *= arr[i][j];
+        }
+    }
+    // Only change code above this line
+    return product;
 }
 
-// Setup
-var myArr = [2, 3, 4, 5, 6];
+// Modify values below to test your code
+multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
 
-// Only change code below this line
-
-var total = 0;
-
-for (var i = 0; i < myArr.length; i++) {
-    total += myArr[i];
-}
