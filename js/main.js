0cda439d07c3cda6e1e6d79cbdf9b58e041ae64e
SrdjanSin <mp4065@gmail.com>
2017-01-29 23:54:13 +0100

    codeCamp: Iterate Through an Array with a For Loop

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
-//Instructions
-//Use a for loop to work to push the values 1 through 5 onto myArray.
+// Instructions
+// Declare and initialize a variable total to 0.
+// Use a for loop to add the value of each element of the myArr array to total.
 // Example
-var ourArray = [];
+var ourArr = [9, 10, 11, 12];
+var ourTotal = 0;
 
-for (var i = 0; i < 5; i++) {
-    ourArray.push(i);
+for (var i = 0; i < ourArr.length; i++) {
+    ourTotal += ourArr[i];
 }
 
 // Setup
-var myArray = [];
+var myArr = [2, 3, 4, 5, 6];
 
-// Only change code below this line.
+// Only change code below this line
 
-for (var i = 1; i <= 5; i++) {
-    myArray.push(i);
+var total = 0;
+
+for (var i = 0; i < myArr.length; i++) {
+    total += myArr[i];
 }
-
-console.log(myArray);
