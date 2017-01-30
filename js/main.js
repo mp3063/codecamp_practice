b22ab7bdaeb3f22b7bac190b0abced887c9f82a6
SrdjanSin <mp4065@gmail.com>
2017-01-30 01:32:58 +0100

    codeCamp: Profile Lookup

--- js/main.js
+++ js/main.js
@@ -1,19 +1,51 @@
-// Instructions
-// Modify function multiplyAll
-// so that it multiplies the product variable by each number in the sub-arrays of arr
-
-function multiplyAll(arr) {
-    var product = 1;
+// We have an array of objects representing different people in our contacts lists.
+// A lookUpProfile function that takes firstName and a property (prop) as arguments has been pre-written for you.
+// The function should check if firstName is an actual contact's firstName and the given property (prop) is a property of  that contact.
+// If both are true, then return the "value" of that property.
+// If firstName does not correspond to any contacts then return "No such contact"
+// If prop does not correspond to any valid properties then return "No such property"
+//Setup
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["Javascript", "Gaming", "Foxes"]
+    }
+];
+function lookUpProfile(firstName, prop)
+{
     // Only change code below this line
-    for (var i = 0; i < arr.length; i++) {
-        for (var j = 0; j < arr[i].length; j++) {
-            product *= arr[i][j];
+    for (var i = 0; i < contacts.length; i++) {
+        if (contacts[i].firstName === firstName) {
+            if (contacts[i].hasOwnProperty(prop)) {
+                return contacts[i][prop];
+            } else {
+                return "No such property";
+            }
         }
     }
+    return "No such contact";
     // Only change code above this line
-    return product;
 }
-
-// Modify values below to test your code
-multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
+// Change these values to test your function
+console.log(lookUpProfile("Sherlock", "likes"));
 
