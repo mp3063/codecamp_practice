90e9335e536a1ead75388f4ab686368a00db6207
SrdjanSin <mp4065@gmail.com>
2017-01-30 21:24:30 +0100

    codeCamp: Convert HTML Entities

--- js/main.js
+++ js/main.js
@@ -1,20 +1,11 @@
-//Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
-//In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
-// The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
-function uniteUnique(arr)
+// Convert the characters &, <, >, " (double quote), and ' (apostrophe),
+// in a string to their corresponding HTML entities.
+// Remember to use Read-Search-Ask if you get stuck.
+// Try to pair program. Write your own code.
+//   ' = &apos;
+function convertHTML(str)
 {
-    var sorted = [];
-    for (var i = 0; i < arguments.length; i++) {
-        sorted.push(arguments[i]);
-    }
-    var flat = sorted.reduce(function (a, b)
-    {
-        return a.concat(b);
-    });
-    return flat.filter(function (item, index, inputArray)
-    {
-        return inputArray.indexOf(item) == index;
-    });
+    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
+                      .replace(/'/g, '&apos;').replace(/"/g, '&​quot;');
 }
-console.log(uniteUnique([1, 3, 2], [1, [5]], [2, [4]]));
-
+console.log(convertHTML('Stuff in "quotation marks"'));
