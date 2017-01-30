bbc1d9353e2113204133dc9409a8009fecbc3d84
SrdjanSin <mp4065@gmail.com>
2017-01-30 20:55:59 +0100

    codeCamp: Sorted Union

--- js/main.js
+++ js/main.js
@@ -1,51 +1,20 @@
-// We have an array of objects representing different people in our contacts lists.
-// A lookUpProfile function that takes firstName and a property (prop) as arguments has been pre-written for you.
-// The function should check if firstName is an actual contact's firstName and the given property (prop) is a property of  that contact.
-// If both are true, then return the "value" of that property.
-// If firstName does not correspond to any contacts then return "No such contact"
-// If prop does not correspond to any valid properties then return "No such property"
-//Setup
-var contacts = [
-    {
-        "firstName": "Akira",
-        "lastName": "Laine",
-        "number": "0543236543",
-        "likes": ["Pizza", "Coding", "Brownie Points"]
-    },
-    {
-        "firstName": "Harry",
-        "lastName": "Potter",
-        "number": "0994372684",
-        "likes": ["Hogwarts", "Magic", "Hagrid"]
-    },
-    {
-        "firstName": "Sherlock",
-        "lastName": "Holmes",
-        "number": "0487345643",
-        "likes": ["Intriguing Cases", "Violin"]
-    },
-    {
-        "firstName": "Kristian",
-        "lastName": "Vos",
-        "number": "unknown",
-        "likes": ["Javascript", "Gaming", "Foxes"]
-    }
-];
-function lookUpProfile(firstName, prop)
+//Write a function that takes two or more arrays and returns a new array of unique values in the order of the original provided arrays.
+//In other words, all values present from all arrays should be included in their original order, but with no duplicates in the final array.
+// The unique numbers should be sorted by their original order, but the final array should not be sorted in numerical order.
+function uniteUnique(arr)
 {
-    // Only change code below this line
-    for (var i = 0; i < contacts.length; i++) {
-        if (contacts[i].firstName === firstName) {
-            if (contacts[i].hasOwnProperty(prop)) {
-                return contacts[i][prop];
-            } else {
-                return "No such property";
-            }
-        }
+    var sorted = [];
+    for (var i = 0; i < arguments.length; i++) {
+        sorted.push(arguments[i]);
     }
-    return "No such contact";
-    // Only change code above this line
+    var flat = sorted.reduce(function (a, b)
+    {
+        return a.concat(b);
+    });
+    return flat.filter(function (item, index, inputArray)
+    {
+        return inputArray.indexOf(item) == index;
+    });
 }
-// Change these values to test your function
-console.log(lookUpProfile("Sherlock", "likes"));
+console.log(uniteUnique([1, 3, 2], [1, [5]], [2, [4]]));
 
