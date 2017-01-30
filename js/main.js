3c9e72a62435c595004d817fa623534ac8e74836
SrdjanSin <mp4065@gmail.com>
2017-01-31 00:46:23 +0100

    codeCamp: Sum All Odd Fibonacci Numbers

--- js/main.js
+++ js/main.js
@@ -1,19 +1,27 @@
-// Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
-//Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
-function spinalCase(str)
+// Given a positive integer num,
+// return the sum of all odd Fibonacci numbers that are less than or equal to num.
+// The first two numbers in the Fibonacci sequence are 1 and 1.
+// Every additional number in the sequence is the sum of the two previous numbers.
+// The first six numbers of the Fibonacci sequence are 1, 1, 2, 3, 5 and 8.
+//For example, sumFibs(10) should return 10 because all odd Fibonacci numbers less than 10 are 1, 1, 3, and 5.
+function sumFibs(num)
 {
-    var regex = /(?=[A-Z\s_"])/;
-    var regex1 = /(?=[\s_"])/;
-    var splitted = str.split(regex);
-    var clean = [];
-    for (var i = 0; i < splitted.length; i++) {
-        var cle = splitted[i].trim();
-        if (regex1.test(cle) === false) {
-            if ('' !== cle) {
-                clean.push(cle);
-            }
+    var fibon = [1, 1];
+    for (var i = 0; i <= num; i++) {
+        if (fibon[fibon.length - 1] <= num) {
+            fibon.push(fibon[i] + fibon[fibon.length - 1]);
         }
     }
-    return clean.join('-').toLowerCase();
+    var arr = [1];
+    for (var k = 1; k <= fibon.length; k++) {
+        if (fibon[k] % 2 == 1 && fibon[k] <= num) {
+            arr.push(fibon[k]);
+        }
+    }
+    var sum = 0;
+    for (var j = 0; j < arr.length; j++) {
+        sum += arr[j];
+    }
+    return sum;
 }
-console.log(spinalCase("Teletubbies say Eh-oh"));
+console.log(sumFibs(4));
