bc27b251a96a3ccc48a491db6b9ad6124bf326a6
SrdjanSin <mp4065@gmail.com>
2017-01-30 23:39:39 +0100

    codeCamp: Spinal Tap Case

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
-// Convert the characters &, <, >, " (double quote), and ' (apostrophe),
-// in a string to their corresponding HTML entities.
-// Remember to use Read-Search-Ask if you get stuck.
-// Try to pair program. Write your own code.
-//   ' = &apos;
-function convertHTML(str)
+// Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+//Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+function spinalCase(str)
 {
-    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
-                      .replace(/'/g, '&apos;').replace(/"/g, '&​quot;');
+    var regex = /(?=[A-Z\s_"])/;
+    var regex1 = /(?=[\s_"])/;
+    var splitted = str.split(regex);
+    var clean = [];
+    for (var i = 0; i < splitted.length; i++) {
+        var cle = splitted[i].trim();
+        if (regex1.test(cle) === false) {
+            if ('' !== cle) {
+                clean.push(cle);
+            }
+        }
+    }
+    return clean.join('-').toLowerCase();
 }
-console.log(convertHTML('Stuff in "quotation marks"'));
+console.log(spinalCase("Teletubbies say Eh-oh"));
