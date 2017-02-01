2ce0e7c1da7b89f1db10c45e5640961ae544ccda
SrdjanSin <mp4065@gmail.com>
2017-02-01 23:21:40 +0100

    codeCamp: Everything Be True

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
-// Return an English translated sentence of the passed binary string.
-// The binary string will be space separated.
-function binaryAgent(str)
+// Check if the predicate (second argument) is truthy
+// on all elements of a collection (first argument).
+// Remember, you can access object properties
+// through either dot notation or [] notation.
+function truthCheck(collection, pre)
 {
-    var splitted = str.split(" ");
-    return splitted.map(function (item)
+    return collection.every(function (item)
     {
-        return String.fromCharCode(parseInt(item, 2));
-    }, []).join("");
+        return item.hasOwnProperty(pre) && Boolean(item[pre]);
+    });
 }
-console.log(binaryAgent(
-    "01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111"));
+console.log(truthCheck([
+    {
+        "user": "Tinky-Winky", "sex": "male"
+    }, {
+        "user": "Dipsy", "sex": "male"
+    }, {
+        "user": "Laa-Laa", "sex": "female"
+    }, {
+        "user": "Po", "sex": "female"
+    }
+], "sex"));
