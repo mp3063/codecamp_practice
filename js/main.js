285629ebe5ffa1b29a4e6e505c62bb7ffad1ee19
SrdjanSin <mp4065@gmail.com>
2017-02-01 22:30:51 +0100

    codeCamp: Steamroller

--- js/main.js
+++ js/main.js
@@ -1,21 +1,14 @@
-// Drop the elements of an array (first argument),
-// starting from the front, until the predicate (second argument) returns true.
-// The second argument, func, is a function you'll use to test
-// the first elementsof the array to decide if you should drop it or not.
-// Return the rest of the array, otherwise return an empty array.
-function dropElements(arr, func)
+// Flatten a nested array. You must account for varying levels of nesting.
+// Remember to use Read-Search-Ask if you get stuck.
+// Try to pair program. Write your own code.
+function steamrollArray(arr)
 {
-    var length = arr.length;
-    for (var i = 0; i < length; i++) {
-        if (func(arr[0])) {
-            break;
-        } else {
-            arr.shift();
+    return arr.reduce(function (a, b)
+    {
+        if (Array.isArray(b)) {
+            return a.concat(steamrollArray(b));
         }
-    }
-    return arr;
+        return a.concat(b);
+    }, []);
 }
-console.log(dropElements([1, 2, 3, 4], function (n)
-{
-    return n > 0;
-}));
+console.log(steamrollArray([1, [2], [3, [[4]]]]));
