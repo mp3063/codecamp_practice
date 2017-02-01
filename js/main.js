0ee8e3fd201949502facd748d677b2603d5881c9
SrdjanSin <mp4065@gmail.com>
2017-02-01 13:17:56 +0100

    codeCamp: Finders Keepers

--- js/main.js
+++ js/main.js
@@ -1,33 +1,14 @@
-// Find the smallest common multiple of the provided parameters that can be evenly divided by both,
-// as well as by all sequential numbers in the range between these parameters.
-// The range will be an array of two numbers that will not necessarily be in numerical order.
-// e.g. for 1 and 3 - find the smallest common multiple of both 1 and 3
-// that is evenly divisible by all numbers between 1 and 3.
-function smallestCommons(arr)
+// Create a function that looks through an array (first argument)
+// and returns the first element in the array that passes a truth test (second argument).
+function findElement(arr, func)
 {
-    function sortNumber(a, b)
-    {
-        return a - b;
-    }
-    
-    var sorted = arr.sort(sortNumber);
-    var from = sorted[0];
-    var to = sorted[1];
-    var range = [];
-    for (var i = from; i <= to; i++) {
-        range.push(i);
-    }
-    for (var k = 1; k < range.length; k++) {
-        from = (from * range[k]) / evenlyDivisible(from, range[k]);
-    }
-    return from;
-    function evenlyDivisible(fromNumber, modulo)
-    {
-        if (modulo === 0) {
-            return fromNumber;
-        } else {
-            return evenlyDivisible(modulo, fromNumber % modulo);
+    for (var i = 0; i < arr.length; i++) {
+        if (func(arr[i])) {
+            return arr[i];
         }
     }
 }
-console.log(smallestCommons([5, 10]));
+console.log(findElement([1, 2, 3, 4], function (num)
+{
+    return num % 2 === 0;
+}));
