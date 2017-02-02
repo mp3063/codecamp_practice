8690993dc7b6f83ee0e30a24890f07de1931ecca
SrdjanSin <mp4065@gmail.com>
2017-02-02 01:21:41 +0100

    codeCamp: Validate US Telephone Numbers

--- js/main.js
+++ js/main.js
@@ -1,37 +1,23 @@
-// Create a function that sums two arguments together.
-// If only one argument is provided, then return a function
-// that  expects one argument and returns the sum.
-// For example, addTogether(2, 3) should return 5,
-// and addTogether(2) should return a function.
-// Calling this returned function with
-// a single argument will then return the sum:
-// var sumTwoAnd = addTogether(2);
-// sumTwoAnd(3) returns 5.
-// If either argument isn't a valid number, return undefined.
-function addTogether()
+// Return true if the passed string is a valid US phone number.
+// The user may fill out the form field any way they choose
+// as long as it is a valid US number.
+// The following are examples of valid formats for US numbers (refer to the tests below for other variants):
+// 555-555-5555
+// (555)555-5555
+// (555) 555-5555
+// 555 555 5555
+// 5555555555
+// 1 555 555 5555
+// For this challenge you will be presented with a string
+// such as 800-692-7753 or 8oo-six427676;laskdjf.
+// Your job is to validate or reject the US phone number
+// based on any combination of the formats provided above.
+// The area code is required. If the country code is provided,
+// you must confirm that the country code is 1.
+// Return true if the string is a valid US phone number; otherwise return false.
+function telephoneCheck(str)
 {
-    var args = Array.from(arguments);
-    var trueOrFalse = args.every(function (item)
-    {
-        return typeof item == 'number';
-    });
-    if (trueOrFalse === false) {
-        return undefined;
-    }
-    if (args.length > 1) {
-        return args.reduce(function (a, b)
-        {
-            return (a + b);
-        });
-    }
-    if (args.length == 1) {
-        return function sumTwoAnd(num)
-        {
-            if (typeof num !== 'number') {
-                return undefined;
-            }
-            return num + args[0];
-        };
-    }
+    var regex = new RegExp(/^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/);
+    return regex.test(str);
 }
-console.log(addTogether(2)([3]));
+console.log(telephoneCheck("1 456 789 4444"));
