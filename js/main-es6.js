71a5dc0dcd5084a18ec43426d20329aead125b4d
SrdjanSin <mp4065@gmail.com>
2017-02-18 01:10:54 +0100

    codeCamp: Pomodoro Clock - jQuery

--- js/main-es6.js
+++ js/main-es6.js
@@ -7,13 +7,16 @@ $(document).ready(function () {
     let workValueField = $(".work-value");
     let restValueField = $(".break-value");
     let pause          = false;
+    let clicked = false;
     let counting;
 
     // Update values on click
-    $(addWork).on("click", function () {
+    let some = $(addWork).on("click", function () {
         let value = parseInt(workValueField.text()) + 1;
         workValueField.text(value);
+        return value[0];
     });
+    console.log(some);
     $(subWork).on("click", function () {
         let value = parseInt(workValueField.text()) - 1;
         workValueField.text(value);
@@ -45,34 +48,20 @@ $(document).ready(function () {
         return minutes + ":" + seconds;
     }
 
-    let count     = parseInt($(workValueField).text()) * 60;
-    let rest      = parseInt($(restValueField).text()) * 60;
-    let workCount = count;
 
-    // Timer toggle Session or Break time if isSession is true or false
-    function timer() {
-        if (prop.isSession) {
-            count--;
-            workValueField.text(formatTime(count));
-            pomodoro.html(`<h1>Wooork!!!</h1><h2>${formatTime(count)}</h2>`);
-            if (count == 0) {
-                count          = rest;
-                prop.isSession = false;
-            }
-        } else {
-            count--;
-            restValueField.text(formatTime(count));
-            pomodoro.toggleClass("red");
-            pomodoro.html(`<h1>Break!</h1><h2>${formatTime(count)}</h2>`);
-            if (count == 0) {
-                count          = workCount;
-                prop.isSession = true;
-            }
-        }
-    }
+    let workSeconds;
+    let restSeconds;
+    let workCount;
 
     $(pomodoro).on("click", function () {
         if (pause === false) {
+            if (clicked === false) {
+                // Pass data in seconds on first click and prevent changing data on other clicks...
+                workSeconds = parseInt($(workValueField).text()) * 60;
+                restSeconds = parseInt($(restValueField).text()) * 60;
+                workCount   = workSeconds;
+                clicked     = true;
+            }
             counting = setInterval(timer, 1000);
             pause    = true;
         }
@@ -82,4 +71,25 @@ $(document).ready(function () {
         }
     });
 
+    // Timer toggle Session or Break time if isSession is true or false
+    function timer() {
+        if (prop.isSession) {
+            workSeconds--;
+            workValueField.text(formatTime(workSeconds));
+            pomodoro.html(`<h1>Wooork!!!</h1><h2>${formatTime(workSeconds)}</h2>`);
+            if (workSeconds == 0) {
+                workSeconds    = restSeconds;
+                prop.isSession = false;
+            }
+        } else {
+            workSeconds--;
+            restValueField.text(formatTime(workSeconds));
+            pomodoro.toggleClass("red");
+            pomodoro.html(`<h1>Break!</h1><h2>${formatTime(workSeconds)}</h2>`);
+            if (workSeconds == 0) {
+                workSeconds    = workCount;
+                prop.isSession = true;
+            }
+        }
+    }
 });
--- js/main.js
+++ js/main.js
@@ -1,21 +1,24 @@
 "use strict";
 
 $(document).ready(function () {
-    var pomodoro       = $(".pomodoro-clock");
-    var addWork        = $(".session-plus");
-    var subWork        = $(".session-minus");
-    var addRest        = $(".break-plus");
-    var subRest        = $(".break-minus");
+    var pomodoro = $(".pomodoro-clock");
+    var addWork = $(".session-plus");
+    var subWork = $(".session-minus");
+    var addRest = $(".break-plus");
+    var subRest = $(".break-minus");
     var workValueField = $(".work-value");
     var restValueField = $(".break-value");
-    var pause          = false;
-    var counting       = void 0;
+    var pause = false;
+    var clicked = false;
+    var counting = void 0;
 
     // Update values on click
-    $(addWork).on("click", function () {
+    var some = $(addWork).on("click", function () {
         var value = parseInt(workValueField.text()) + 1;
         workValueField.text(value);
+        return value[0];
     });
+    console.log(some);
     $(subWork).on("click", function () {
         var value = parseInt(workValueField.text()) - 1;
         workValueField.text(value);
@@ -40,46 +43,53 @@ $(document).ready(function () {
     // Format seconds so it returns minutes and seconds
     function formatTime(seconds) {
         var minutes = Math.floor(seconds / 60);
-        seconds     = seconds % 60;
+        seconds = seconds % 60;
         if (seconds < 10) {
             return minutes + ":" + "0" + seconds;
         }
         return minutes + ":" + seconds;
     }
 
-    var count     = parseInt($(workValueField).text()) * 60;
-    var rest      = parseInt($(restValueField).text()) * 60;
-    var workCount = count;
-
-    // Timer toggle Session or Break time if isSession is true or false
-    function timer() {
-        if (prop.isSession) {
-            count--;
-            workValueField.text(formatTime(count));
-            pomodoro.html("<h1>Wooork!!!</h1><h2>" + formatTime(count) + "</h2>");
-            if (count == 0) {
-                count          = rest;
-                prop.isSession = false;
-            }
-        } else {
-            count--;
-            restValueField.text(formatTime(count));
-            pomodoro.toggleClass("red");
-            pomodoro.html("<h1>Break!</h1><h2>" + formatTime(count) + "</h2>");
-            if (count == 0) {
-                count          = workCount;
-                prop.isSession = true;
-            }
-        }
-    }
+    var workSeconds = void 0;
+    var restSeconds = void 0;
+    var workCount = void 0;
 
     $(pomodoro).on("click", function () {
         if (pause === false) {
+            if (clicked === false) {
+                // Pass data in seconds on first click and prevent changing data on other clicks...
+                workSeconds = parseInt($(workValueField).text()) * 60;
+                restSeconds = parseInt($(restValueField).text()) * 60;
+                workCount = workSeconds;
+                clicked = true;
+            }
             counting = setInterval(timer, 1000);
-            pause    = true;
+            pause = true;
         } else if (pause === true) {
             clearInterval(counting);
             pause = false;
         }
     });
+
+    // Timer toggle Session or Break time if isSession is true or false
+    function timer() {
+        if (prop.isSession) {
+            workSeconds--;
+            workValueField.text(formatTime(workSeconds));
+            pomodoro.html("<h1>Wooork!!!</h1><h2>" + formatTime(workSeconds) + "</h2>");
+            if (workSeconds == 0) {
+                workSeconds = restSeconds;
+                prop.isSession = false;
+            }
+        } else {
+            workSeconds--;
+            restValueField.text(formatTime(workSeconds));
+            pomodoro.toggleClass("red");
+            pomodoro.html("<h1>Break!</h1><h2>" + formatTime(workSeconds) + "</h2>");
+            if (workSeconds == 0) {
+                workSeconds = workCount;
+                prop.isSession = true;
+            }
+        }
+    }
 });
